74ffb0ca37e2ed7e222a6f790c6a0240cb8afe11
Isa Omar <isomarov@outlook.com>
2026-08-31 22:48:30 +0400

    2 defe boyudulfdu header

--- src/components/layout/header/TopBar.jsx
+++ src/components/layout/header/TopBar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 const TopBar = () => {
   return (
-    <div className='bg-[#0a1a3c] text-white text-[11px] sm:text-xs md:text-sm'>
-      <div className='flex items-center justify-center gap-x-4 gap-y-1 py-2 px-4 flex-wrap text-center'>
+    <div className='bg-[#0a1a3c] text-white text-sm sm:text-base md:text-lg'>
+      <div className='flex items-center justify-center gap-x-10 gap-y-2 py-7 px-4 flex-wrap text-center'>
         <span className='italic font-serif'>August Style Event</span>
         <span>Extra 30% Off $175+ | 20% Off $125+</span>
         <span className='hidden sm:flex items-center gap-x-4'>
